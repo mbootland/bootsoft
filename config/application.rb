c9fb35ffcb6ed5a3807220ba60a4111285aeb5d0
mbootland <matthew.bootland@gmail.com>
2023-12-08 18:10:20 +0900

    adding config hosts

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,7 @@ module Bootsoft
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.hosts << "bootsoft.net"
+    config.hosts << "bootsoft.org"
   end
 end
